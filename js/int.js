614f17a9faa34549146d7c212519512713f636fb
Salvador-43 <81052538+Salvador-43@users.noreply.github.com>
2021-03-22 12:27:29 -0600

    Create int.js

---
+++ js/int.js
@@ -0,0 +1,9 @@
+firebaseConfig = {
+    apiKey: "AIzaSyDXoT5XF-wQIVHFaYOt9eJBMm7gkSjqa78",
+    authDomain: "autentifica43-tps.firebaseapp.com",
+    projectId: "autentifica43-tps",
+    storageBucket: "autentifica43-tps.appspot.com",
+    messagingSenderId: "160146341487",
+    appId: "1:160146341487:web:8f4c436d235c6863d5579a",
+    measurementId: "G-WH1XSXXWGK"
+  };
