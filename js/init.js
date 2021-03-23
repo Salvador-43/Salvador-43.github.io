d6039a3bf1074aa416dfdd5a8f61f844b5ceab54
Salvador-43 <81052538+Salvador-43@users.noreply.github.com>
2021-03-22 20:05:22 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 
 //@ts-ignore
 firebase.initializeAPP({
-    apiKey: "AIzaSyDXoT5XF-wQIVHFaYOt9eJBMm7gkSjqa78",
-    authDomain: "autentifica43-tps.firebaseapp.com",
-    projectId: "autentifica43-tps",
-    storageBucket: "autentifica43-tps.appspot.com",
-    messagingSenderId: "160146341487",
-    appId: "1:160146341487:web:8f4c436d235c6863d5579a",
-    measurementId: "G-WH1XSXXWGK"
+    apiKey: "AIzaSyA-w36-H591X58gjIbtR7DCrba5SxsbQ2A",
+    authDomain: "autentificas43.firebaseapp.com",
+    projectId: "autentificas43",
+    storageBucket: "autentificas43.appspot.com",
+    messagingSenderId: "828892524349",
+    appId: "1:828892524349:web:5959c459d519841ead1f54",
+    measurementId: "G-CPQD7P81YP"
   });
