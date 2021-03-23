495d3397d29b40501a79c40f40f0a0c59f9395c7
Salvador-43 <81052538+Salvador-43@users.noreply.github.com>
2021-03-22 20:27:35 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
-
-//@ts-ignore
-firebase.initializeAPP({
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
     apiKey: "AIzaSyA-w36-H591X58gjIbtR7DCrba5SxsbQ2A",
     authDomain: "autentificas43.firebaseapp.com",
     projectId: "autentificas43",
